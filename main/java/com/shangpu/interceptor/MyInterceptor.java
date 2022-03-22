66972c9069cede6d3d324236d1ef7367ce5733d1
menuan6666 <45694718@qq.com>
2022-03-22 16:55:02 +0800

    添加拦截器，未登录一律进入login页面

---
+++ main/java/com/shangpu/interceptor/MyInterceptor.java
@@ -0,0 +1,49 @@
+package com.shangpu.interceptor;
+
+import jdk.nashorn.internal.ir.RuntimeNode;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest,
+                             HttpServletResponse httpServletResponse, Object o) throws Exception {
+        if (httpServletRequest.getRequestURI().contains("login")) {
+            return true;
+        }
+        HttpSession session = httpServletRequest.getSession();
+        Object oo = session.getAttribute("user");
+        System.out.println(oo);
+        // 如果用户已登陆也放行
+        if(oo != null) {
+            System.out.println(1);
+            return true;
+        }
+
+        // 用户没有登陆跳转到登陆页面
+        System.out.println(2);
+        httpServletRequest.getRequestDispatcher("/frontend/login").forward(httpServletRequest, httpServletResponse);
+        return false;
+    }
+//        System.out.println("登陆判断");
+//        System.out.println(httpServletRequest.getSession().getAttribute("user"));
+//        if (httpServletRequest.getSession().getAttribute("user") == null) {
+//            System.out.println("登陆不为空");
+//            return true;
+//        }
+//        httpServletResponse.sendRedirect("/frontend/login");
+//        return false;}
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("afterCompletion");
+    }
+}
--- main/java/com/shangpu/listener/MyContextListener.java
+++ main/java/com/shangpu/listener/MyContextListener.java
@@ -5,7 +5,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+//这玩意没啥用啊  我看着  目前为止   他哪里也用不到  就是在关闭tomcat的时候提前关闭tomcat的服务，要不我得tomcat报错0.0
 public class MyContextListener implements ServletContextListener {
 
     @Override
