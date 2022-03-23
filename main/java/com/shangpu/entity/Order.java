38ff262330d63d33b517c8105b4545d20775faea
menuan6666 <45694718@qq.com>
2022-03-23 17:33:15 +0800

    优化Order实体类和表

--- main/java/com/shangpu/entity/Order.java
+++ main/java/com/shangpu/entity/Order.java
@@ -15,6 +15,8 @@ public class Order {
     private int orderType;
     //商品名
     private String productName;
+    //是否完成 0未完成 1已完成
+    private int  status;
     //个人信息
     private PersonInfo personInfo;
 }
